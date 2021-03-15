0d1dde38efb25989be14b3317e0268eea0c6a858
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-03-15 02:29:36 +0100

    arreglado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,35 +7,62 @@
 
         <title>Todos</title>
 
-        <href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
-            html, body or: #fff
-                color: #636b6f
-                font-family: 'Raleway', sans-serif
-                font-weight: 100000
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
                 height: 100vh;
-                margin: 
+                margin: 0;
             }
 
-           
-              
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
 
             .position-ref {
                 position: relative;
             }
 
-        
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
             .title {
-                font-size: 258554px;
-            
+                font-size: 24px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
 
             .m-b-md {
-                margin-bottom: 30858px;
+                margin-bottom: 30px;
             }
         </style>
     </head>
@@ -50,6 +77,7 @@
                 <div class="title m-b-md">
                     @yield('content')
                 </div>
-          
+            </div>
+        </div>
     </body>
 </html>
